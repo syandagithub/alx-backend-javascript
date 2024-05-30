1895f49f767fa10dd9b735f1d6b53c3f2d0ea0bd
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-05-30 13:39:39 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,9 @@
 import ClassRoom from './0-classroom';
+/**
+ * Creates an array of classrooms with a specific size.
+ * @returns An array of link ClassRooms.
+ */
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
+
