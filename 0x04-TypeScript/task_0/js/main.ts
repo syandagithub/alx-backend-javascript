b55491f0677f185c34735b2dd53794e4ded663b6
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-06-05 20:20:29 +0200

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,23 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+const student1: Student = {
+  firstName: "Siya",
+  lastName: "MN",
+  age: 2,
+  location: "SA,  JHB",
+};
+const student: Student = {
+  firstName: "Zama",
+  lastName: "Shaba",
+  age: 33,
+  location: "SA, PMB",
+};
 
+const studentsList: Array<Student> = [
+  student1,
+  student,
+];
